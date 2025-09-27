b706c12ba2d50faae35140d78e32ddf4e57d036d
pedroeloy <33546209+pedroeloy@users.noreply.github.com>
2025-09-28 00:56:57 +0100

    Update fix_edge_pwas_icon.sh

--- scripts/fix_edge_pwas_icon.sh
+++ scripts/fix_edge_pwas_icon.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 # Fix Microsoft Edge PWAs icons on GNOME Wayland
 # by changing StartupWMClass= in the .desktop files
+#
+#
+
+# Manual Instructions
+# Correct Edge Webapp Icons when running webapps (Teams for example)
+# Press <kbd>Alt</kbd>+<kbd>F2</kbd>, type lg, and hit Enter.
+# Go to the Windows tab.
+# Find your Edge webapp window → it will show an app entry like:
+# app: microsoft-edge-webapp-Example-com.desktop
+# That string (microsoft-edge-webapp-Example-com) is what GNOME expects for StartupWMClass
+# and edit the corresponding .desktop file in ~/.local/share/applications
 
 
 APP_DIR="$HOME/.local/share/applications"
