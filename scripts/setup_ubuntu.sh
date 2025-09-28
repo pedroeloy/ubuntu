6afab731e8c742e731a8eda10d52fdeb889c3e3a
pedroeloy <33546209+pedroeloy@users.noreply.github.com>
2025-09-28 19:24:40 +0100

    Update setup_ubuntu.sh

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -168,69 +168,12 @@ sudo apt install microsoft-edge-stable -y
 #sudo apt install xfce-terminal -y
 
 
-##################################################################################################
-# Configure Gdrive on ~/GoogleDrive, execute after the installation:
-# rclone config
-# n) New remote
-# name: gdrive
-# Storage: drive
-# client id: [ENTER]
-# client secret: [ENTER]
-# Scope: 1 - Full access
-# service_account_file: [ENTER]
-# Advanced config: No
-# (Do login in Browser)
-# Auto Config: Yes
-# Shared Drive (Team Drive): No
-# Keep this drive remote: Yes
-# q) Quit Config
-# rclone ls gdrive:
-# rclone mount gdrive: ~/GoogleDrive --vfs-cache-mode writes
-##################################################################################################
-#sudo apt install rclone fuse3 -y
-#mkdir -p ~/GoogleDrive
-#mkdir -p ~/.config/systemd/user
-#nano ~/.config/systemd/user/rclone-gdrive.service
-
-# cat  <<EOF > ~/.config/systemd/user/rclone-gdrive.service
-#
-#[Unit]
-#Description=Mount Google Drive via rclone
-#After=network-online.target
-#Wants=network-online.target
-#
-#[Service]
-#ExecStart=/usr/bin/rclone mount gdrive: %h/GoogleDrive \
-#    --vfs-cache-mode writes \
-#    --dir-cache-time 1h \
-#    --poll-interval 15s \
-#    --umask 002 \
-#    --uid 1000 --gid 1000 \
-#    --allow-other
-#Restart=on-failure
-#Environment=PATH=/usr/bin:/bin:/usr/sbin:/sbin
-#
-#[Install]
-#WantedBy=default.target
-#EOF
-
-#systemctl --user daemon-reload
-#systemctl --user enable rclone-gdrive
-#systemctl --user start rclone-gdrive
-
-#fusermount3 -u ~/GoogleDrive
-#rclone listremotes
-#rclone sync ~/Documents gdrive:DocumentsBackup
-
-
-
 
 ##################################################################################################
 # Install Gnome Software (including flatpak support)  and then reboot!
 ##################################################################################################
 #sudo apt install gnome-software gnome-software-plugin-flatpak -y
 #flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
 
 
 # Install extras including microsoft fonts and extra codecs, must be run manually to accept EULAs.
