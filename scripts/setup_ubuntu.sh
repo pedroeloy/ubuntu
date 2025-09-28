76e5f0bb46edc8f5b10dadd8f219b1aa9b2e8f69
pedroeloy <33546209+pedroeloy@users.noreply.github.com>
2025-09-28 09:39:47 +0100

    Update setup_ubuntu.sh

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -156,18 +156,6 @@ sudo apt install microsoft-edge-stable -y
 #sudo systemctl enable --now input-remapper
 
 
-
-##################################################################################################
-# Correct Edge Webapp Icons when running webapps (Teams for example)
-# Press <kbd>Alt</kbd>+<kbd>F2</kbd>, type lg, and hit Enter.
-# Go to the Windows tab.
-# Find your Edge webapp window → it will show an app entry like:
-# app: microsoft-edge-webapp-Example-com.desktop
-# That string (microsoft-edge-webapp-Example-com) is what GNOME expects for StartupWMClass
-# and edit the corresponding .desktop file in ~/.local/share/applications
-##################################################################################################
-
-
 ##################################################################################################
 # Install Geany Text Editor and get colour themes!
 ##################################################################################################
